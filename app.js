9fe68920b8150808b4bb87ed0d836d8847ca19f3
Giorgos kalmoukis <giorgoskalmoukis@gmail.com>
2020-07-22 18:17:28 +0300

    Add feature copy markdown to clipboard

--- app.js
+++ app.js
@@ -6,9 +6,25 @@ new Vue({
         }
     },
     methods:{
-        save(){
+        copy(){
+
             console.log(document.getElementById("markdown-content").value)
-        }
+            
+            let testingCodeToCopy = document.querySelector('#markdown-content')
+            testingCodeToCopy.setAttribute('type', 'text')
+            testingCodeToCopy.select()
+            try {
+                var successful = document.execCommand('copy');
+                var msg = successful ? 'successful' : 'unsuccessful';
+                alert('Testing code was copied ' + msg);
+            } catch (err) {
+                alert('Oops, unable to copy');
+            }
+
+            /* unselect the range */
+            testingCodeToCopy.setAttribute('type', 'hidden')
+            window.getSelection().removeAllRanges()
+            },
     },
     filters: {
         marked: marked
